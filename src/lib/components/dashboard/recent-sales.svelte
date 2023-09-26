aefa5a25681bbe7f6e04c664243755b74baf6deb
149segolte <37300847+149segolte@users.noreply.github.com>
2023-09-26 20:33:57 +0530

    Fixed Real time logs

--- src/lib/components/dashboard/recent-sales.svelte
+++ src/lib/components/dashboard/recent-sales.svelte
@@ -2,16 +2,35 @@
 	import * as Table from '$lib/components/ui/table';
 	import { onMount } from 'svelte';
 
-	const url = 'https://flowing-teal-terribly.ngrok-free.app';
+	const url = 'https://winter-thunder-3785.fly.dev';
 
+	let loaded = false;
 	let logs = [];
 
 	onMount(() => {
 		const interval = setInterval(async () => {
-			let res = await fetch(`${url}/log`);
+			let res = await fetch(`${url}/log`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json'
+				}
+			});
 			const data = await res.json();
 			console.log(data);
-			logs = data.logs;
+			logs = [];
+			for (let i = 0; i < data.length; i++) {
+				logs.push({
+					date: data[i].date,
+					time: data[i].time,
+					level: data[i].level.slice(1, -1),
+					header: data[i].header,
+					message: data[i].message,
+					address: data[i].address,
+					retention: data[i].retention
+				});
+			}
+			loaded = true;
 		}, 5000);
 
 		return () => clearInterval(interval);
@@ -24,27 +43,35 @@
 			<Table.Head class="w-[240px]">Request Time</Table.Head>
 			<Table.Head class="w-[100px]">Level</Table.Head>
 			<Table.Head>Log</Table.Head>
-			<Table.Head class="text-right">Origin</Table.Head>
+			<Table.Head>Origin</Table.Head>
+			<Table.Head class="text-right">Retention</Table.Head>
 		</Table.Row>
 	</Table.Header>
 	<Table.Body>
-		{#each logs as log}
+		{#if loaded}
+			{#each logs as log}
+				<Table.Row>
+					<Table.Cell class="font-medium">{log.date} {log.time}</Table.Cell>
+					<Table.Cell class="font-medium">
+						{#if log.level === 'INFO'}
+							<span class="text-green-500">{log.level}</span>
+						{:else if log.level === 'WARN'}
+							<span class="text-yellow-500">{log.level}</span>
+						{:else if log.level === 'ERROR'}
+							<span class="text-red-500">{log.level}</span>
+						{:else}
+							<span class="text-blue-500">{log.level}</span>
+						{/if}
+					</Table.Cell>
+					<Table.Cell>{JSON.stringify(log.header)}{JSON.stringify(log.message)}</Table.Cell>
+					<Table.Cell>{log.address}</Table.Cell>
+					<Table.Cell class="text-right">{log.retention}</Table.Cell>
+				</Table.Row>
+			{/each}
+		{:else}
 			<Table.Row>
-				<Table.Cell class="font-medium">{log.time}</Table.Cell>
-				<Table.Cell class="font-medium">
-					{#if log.level === 'INFO'}
-						<span class="text-green-500">{log.level}</span>
-					{:else if log.level === 'WARN'}
-						<span class="text-yellow-500">{log.level}</span>
-					{:else if log.level === 'ERROR'}
-						<span class="text-red-500">{log.level}</span>
-					{:else}
-						<span class="text-blue-500">{log.level}</span>
-					{/if}
-				</Table.Cell>
-				<Table.Cell>{JSON.stringify(log.headers)}{JSON.stringify(log.log)}</Table.Cell>
-				<Table.Cell class="text-right">{log.headers['x-forwarded-for']}</Table.Cell>
+				<Table.Cell class="text-center">Loading...</Table.Cell>
 			</Table.Row>
-		{/each}
+		{/if}
 	</Table.Body>
 </Table.Root>
